1484ea579ee84c9197f99f1e8f4299d6c339f2ac
Matt Moore <matt@mattmoore.io>
2020-03-12 20:51:17 -0500

    Add parser combinator imports.

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,5 +6,8 @@ import Text.Parsec.Expr
 import Text.Parsec.Token
 import Text.Parsec.Language
 
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+
 parse :: String -> String
 parse source = source
