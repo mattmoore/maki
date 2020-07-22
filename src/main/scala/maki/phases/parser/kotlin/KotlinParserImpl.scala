a6abf5c62304c8fe4d3a13a031a2978dac17f260
Matt Moore <matt@mattmoore.io>
2020-07-22 01:35:30 -0500

    Reformat and remove initParse.

--- src/main/scala/maki/phases/parser/kotlin/KotlinParserImpl.scala
+++ src/main/scala/maki/phases/parser/kotlin/KotlinParserImpl.scala
@@ -4,22 +4,33 @@ import maki.kotlin.{KotlinLexer, KotlinParser}
 import org.antlr.v4.runtime.{CharStreams, CodePointCharStream, CommonTokenStream}
 
 object KotlinParserImpl {
-  private val charStream = (source: String) => CharStreams.fromString(source)
-  private val lexer = (stream: CodePointCharStream) => new KotlinLexer(stream)
-  private val tokenStream = (lexer: KotlinLexer) => new CommonTokenStream(lexer)
-  private val parser = (tokenStream: CommonTokenStream) => new KotlinParser(tokenStream)
-  private val visitor = (source: String) => KotlinVisitor(KotlinParserImpl(source).kotlinFile)
+  private val charStream =
+    (source: String) => CharStreams.fromString(source)
 
-  private def initParser = (
+  private val lexer =
+    (stream: CodePointCharStream) => new KotlinLexer(stream)
+
+  private val tokenStream =
+    (lexer: KotlinLexer) => new CommonTokenStream(lexer)
+
+  private val parser =
+    (tokenStream: CommonTokenStream) => new KotlinParser(tokenStream)
+
+  private val visitor =
+    (source: String) => KotlinVisitor(KotlinParserImpl(source).kotlinFile)
+
+  def apply(source: String) = (
     charStream
       andThen lexer
       andThen tokenStream
       andThen parser
-    )
+    ) (source)
 
-  def apply(source: String) = initParser(source)
+  def parse(source: String) =
+    visitor(source).ast
 
-  def parse(source: String) = visitor(source).ast
-
-  def parseLisp(source: String) = KotlinParserImpl(source).kotlinFile.toStringTree(KotlinParserImpl(source))
+  def parseLisp(source: String) =
+    KotlinParserImpl(source)
+      .kotlinFile
+      .toStringTree(KotlinParserImpl(source))
 }
