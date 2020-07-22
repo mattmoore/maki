6a9c67bdd25e53eb769af230c8d38aa27905f48a
Matt Moore <matt@mattmoore.io>
2020-07-22 01:51:59 -0500

    Add ParseTreeProperty for storing context values.

--- src/main/scala/maki/phases/parser/kotlin/KotlinVisitor.scala
+++ src/main/scala/maki/phases/parser/kotlin/KotlinVisitor.scala
@@ -2,42 +2,43 @@ package maki.phases.parser.kotlin
 
 import maki.kotlin.{KotlinParser, KotlinParserBaseVisitor}
 import maki.phases.analysis.TypeResolver
-import maki.phases.parser.kotlin.ast.{KtFile, KtFunction, KtProperty}
-import org.antlr.v4.runtime.tree.ParseTree
-
-class KotlinVisitor extends KotlinParserBaseVisitor[Unit] {
-  var ast: KtFile = new KtFile
-
-  override def visitPropertyDeclaration(ctx: KotlinParser.PropertyDeclarationContext): Unit = {
-    ast.nodes.addOne(
-      new KtProperty(
-        name = ctx.variableDeclaration.getText,
-        expression = ctx.expression.getText,
-        dataType = TypeResolver.inferType(ctx.expression.getText)
-      )
-    )
-  }
-
-  override def visitFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext): Unit = {
-    ast.nodes.addOne(
-      new KtFunction(
-        name = ctx.simpleIdentifier.getText,
-        `type` = if (ctx.`type` != null) ctx.`type`.getText else "",
-        functionBody = ctx.functionBody.getText
-      )
-    )
-    this.visitFunctionBody(ctx.functionBody)
-  }
-
-  override def visitFunctionBody(ctx: KotlinParser.FunctionBodyContext): Unit = {
-    super.visitFunctionBody(ctx)
-  }
-}
+import maki.phases.parser.kotlin.ast.{ASTNode, KtFile, KtFunction, KtProperty}
+import org.antlr.v4.runtime.tree.{ParseTree, ParseTreeProperty}
 
 object KotlinVisitor {
   def apply(tree: ParseTree): KotlinVisitor = {
     val visitor = new KotlinVisitor()
     visitor.visit(tree)
     visitor
+  }
+}
+
+class KotlinVisitor extends KotlinParserBaseVisitor[Unit] {
+  var ast: KtFile = new KtFile
+  var values = new ParseTreeProperty[ASTNode]
+
+  override def visitPropertyDeclaration(ctx: KotlinParser.PropertyDeclarationContext): Unit = {
+    val property = new KtProperty(
+      name = ctx.variableDeclaration.getText,
+      expression = ctx.expression.getText,
+      dataType = TypeResolver.inferType(ctx.expression.getText)
+    )
+    values.put(ctx, property)
+    ast.nodes.addOne(property)
+  }
+
+  override def visitFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext): Unit = {
+    val function = new KtFunction(
+      name = ctx.simpleIdentifier.getText,
+      `type` = if (ctx.`type` != null) ctx.`type`.getText else "",
+      functionBody = ctx.functionBody.getText
+    )
+    ast.nodes.addOne(function)
+    values.put(ctx, function)
+    this.visitFunctionBody(ctx.functionBody)
+  }
+
+  override def visitFunctionBody(ctx: KotlinParser.FunctionBodyContext): Unit = {
+    super.visitFunctionBody(ctx)
   }
 }
