82e7c715b79484b439226a61d6994b97f3d22725
Matt Moore <matt@mattmoore.io>
2020-12-17 11:53:59 -0600

    Fix AssignmentSpec.

--- src/test/scala/maki/kotlin/AssignmentSpec.scala
+++ src/test/scala/maki/kotlin/AssignmentSpec.scala
@@ -6,41 +6,44 @@ import org.scalatest.matchers.should.Matchers
 
 class AssignmentSpec extends AnyFunSpec with Matchers {
   describe("parse") {
-//    describe("when integer declaration") {
-//      val source = """val x = 5"""
+    describe("when integer declaration") {
+      val source = """val x = 5"""
 
-//      it("constructs an AST") {
-//        KotlinParserImpl.parse(source).nodes(0) should have(
-//          Symbol("name")("x"),
-//          Symbol("expression")("5"),
-//          Symbol("dataType")("Int")
-//        )
-//        KotlinParser(source)
-//      }
-//    }
+      it("constructs an AST") {
+        val ast = KotlinParser.parse(source)
+        ast.children(0) should have(
+          Symbol("name")("x"),
+          Symbol("expression")("5"),
+          Symbol("dataType")("Int")
+        )
+        KotlinParser(source)
+      }
+    }
 
-//    describe("when string declaration") {
-//      val source = """val firstName = "Matt""""
-//
-//      it("constructs an AST") {
-//        KotlinParser.parse(source).nodes(0) should have(
-//          Symbol("name")("firstName"),
-//          Symbol("expression")(""""Matt""""),
-//          Symbol("dataType")("String")
-//        )
-//      }
-//    }
-//
-//    describe("when empty string declaration") {
-//      val source = """val firstName = """""
-//
-//      it("constructs an AST") {
-//        KotlinParser.parse(source).nodes(0) should have(
-//          Symbol("name")("firstName"),
-//          Symbol("expression")("\"\""),
-//          Symbol("dataType")("String")
-//        )
-//      }
-//    }
+    describe("with string declaration") {
+      val source = """val firstName = "Matt""""
+
+      it("constructs an AST") {
+        val ast = KotlinParser.parse(source)
+        ast.children(0) should have(
+          Symbol("name")("firstName"),
+          Symbol("expression")(""""Matt""""),
+          Symbol("dataType")("String")
+        )
+      }
+    }
+
+    describe("with empty string declaration") {
+      val source = """val firstName = """""
+
+      it("constructs an AST") {
+        val ast = KotlinParser.parse(source)
+        ast.children(0) should have(
+          Symbol("name")("firstName"),
+          Symbol("expression")("\"\""),
+          Symbol("dataType")("String")
+        )
+      }
+    }
   }
 }
