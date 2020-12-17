2d12183ebe574f16a4fea6f1031478b185a2ecb5
Matt Moore <matt@mattmoore.io>
2020-12-17 12:02:13 -0600

    Tidy up FunctionSpec.

--- src/test/scala/maki/kotlin/FunctionSpec.scala
+++ src/test/scala/maki/kotlin/FunctionSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.matchers.should.Matchers
 
 class FunctionSpec extends AnyFunSpec with Matchers {
   describe("parse") {
-    describe("function without parameters") {
+    describe("expression function without parameters") {
       val source = """fun hello() = "Hello Maki!""""
 
       it("constructs an AST") {
@@ -20,20 +20,7 @@ class FunctionSpec extends AnyFunSpec with Matchers {
       }
     }
 
-    describe("function with expression") {
-      val source = """fun hello() = "Hello Maki!""""
-
-      it("constructs an AST") {
-        val ast = KotlinParser.parse(source)
-        ast.children(0) should have(
-          Symbol("name")("hello"),
-          Symbol("type")(""),
-          Symbol("functionBody")(KtFunctionBody("", """"Hello Maki!""""))
-        )
-      }
-    }
-
-    describe("function with parameters") {
+    describe("expression function with parameters") {
       val source = """fun hello(name: String) = "Hello ${name}""""
 
       it("constructs an AST") {
@@ -46,7 +33,7 @@ class FunctionSpec extends AnyFunSpec with Matchers {
       }
     }
 
-    describe("function with return type") {
+    describe("expression function with parameter and return type") {
       val source = """fun hello(name: String): String = "Hello ${name}""""
 
       it("constructs an AST") {
