0d8f23dd9da18a7327fe0d33bbc62d409d9fca99
Matt Moore <matt@mattmoore.io>
2021-01-06 02:58:29 -0500

    Change ASTNode back to trait.

--- src/main/scala/maki/languages/kotlin/ast/ASTNode.scala
+++ src/main/scala/maki/languages/kotlin/ast/ASTNode.scala
@@ -4,10 +4,10 @@ import org.antlr.v4.runtime.ParserRuleContext
 
 import scala.collection.mutable.ListBuffer
 
-abstract class ASTNode(
-  var parent: ASTNode = null,
-  var prevSibling: ASTNode = null,
-  var nextSibling: ASTNode = null,
-  var children: ListBuffer[ASTNode] = ListBuffer.empty,
+trait ASTNode {
+  var parent: ASTNode = null
+  var prevSibling: ASTNode = null
+  var nextSibling: ASTNode = null
+  var children: ListBuffer[ASTNode] = ListBuffer.empty
   var context: ParserRuleContext = null
-)
+}
